cab2a9130fad18a181949ed1492a8187b0649d10
omerjanjua <janjuaomer@gmail.com>
2018-06-17 16:47:30 +0100

    Created a brand new project

---
+++ JLTechTest/JLTechTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JLTechTest
+//
+//  Created by Omer Janjua on 17/06/2018.
+//  Copyright © 2018 Omer Janjua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
