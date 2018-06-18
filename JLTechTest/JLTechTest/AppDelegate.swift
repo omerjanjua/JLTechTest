79c3ffcbe7e3ca6a4e14dc08deb010916bbf043f
omerjanjua <janjuaomer@gmail.com>
2018-06-18 15:20:56 +0100

    Restructured project files and updated app delegate to launch the rootViewController

--- JLTechTest/JLTechTest/AppDelegate.swift
+++ JLTechTest/JLTechTest/AppDelegate.swift
@@ -14,7 +14,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        return true
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            let navigationController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! UINavigationController
+            let productCollectionViewController = navigationController.viewControllers[0] as! ProductCollectionViewController
+            let request = JLRequest(baseURL: URL(string: "https://api.johnlewis.com/v1")!)
+            let presenter = ProductPresenter(
+                productInteractor: ProductInteractor(request: request),
+                imageInteractor: ImageInteractor(),
+                view: productCollectionViewController,
+                router: PushingProductDetailRouter(navigationController: navigationController)
+            )
+            productCollectionViewController.eventHandler = presenter
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+            self.window = window
+            return true
     }
 }
 
